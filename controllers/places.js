9abc6176f69aca8ee160a91fb4afe0704e2d7c1f
Eyrma <erotstein@outlook,com>
2023-12-01 21:10:23 -0800

    Added edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,29 +1,28 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const { get } = require("express/lib/response");
+const places = require("../models/places.js");
 
-// GET /places
-router.get('/', (req, res) => {
-  let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: 'public\images\h-thai-ml-food.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: 'public\images\soup.jpg'
-  }]
-  res.render('places/index', { places });
+router.get("/", (req, res) => {
+  res.render("places/index", { places });
 });
 
 router.post("/", (req, res) => {
-  res.send("Create new place");
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 router.get("/new", (req, res) => {
-  res.send("Form page for creating a new place");
+  res.render("places/new");
 });
 
 router.get("/:id", (req, res) => {
@@ -50,4 +49,4 @@ router.delete("/:id/rant/:rantId", (req, res) => {
   res.send("Delete a rant (comment) about a particular place");
 });
 
-module.exports = router
+module.exports = router;
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -23,4 +23,4 @@ function index (data) {
   )
   }
 
-module.exports = index
+module.exports = index;
